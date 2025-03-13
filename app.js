6f4746b2c5b053178164a2f09cc9b053ba5bfac6
Etienne Fuh <36563272+THISmann@users.noreply.github.com>
2025-03-13 20:23:16 +0300

    test_vuln

--- app.js
+++ app.js
@@ -39,3 +39,46 @@ app.get('/external', (req, res) => {
 app.listen(3000, () => {
     console.log('Server running on port 3000');
 });
+
+const express = require('express');
+const lodash = require('lodash');
+const moment = require('moment');
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
+const qs = require('qs');
+
+const app = express();
+const port = 3000;
+
+// Example of a vulnerable lodash usage (CVE-2021-23337)
+const userInput = { prototype: { isAdmin: true } };
+const merged = lodash.merge({}, userInput);
+console.log('Merged object:', merged);
+
+// Example of a vulnerable moment usage (CVE-2022-24785)
+const date = moment().format('YYYY-MM-DD');
+console.log('Current date:', date);
+
+// Example of a vulnerable jsonwebtoken usage (CVE-2022-23529)
+const token = jwt.sign({ user: 'admin' }, 'secret', { algorithm: 'none' });
+console.log('JWT Token:', token);
+
+// Example of a vulnerable axios usage (CVE-2021-3749)
+axios.get('http://example.com', {
+  params: {
+    q: qs.parse('foo=bar&baz=qux')
+  }
+}).then(response => {
+  console.log('Axios response:', response.data);
+}).catch(error => {
+  console.error('Axios error:', error);
+});
+
+// Example of a vulnerable express usage (CVE-2022-24999)
+app.get('/', (req, res) => {
+  res.send('Hello, this is a vulnerable app!');
+});
+
+app.listen(port, () => {
+  console.log(`Vulnerable app listening at http://localhost:${port}`);
+});
